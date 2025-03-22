551a1810f955436af46b68a348b52c103b6a6545
HenriqueBrasilDev <henrique_brasiloficial@outlook.com>
2025-03-22 04:52:08 -0300

    Modal aperfeiçoado, design aperfeiçoado

--- main.js
+++ main.js
@@ -12,3 +12,18 @@ function abrirModal(){
 
     // setTimeout(function () {modal.classList.remove('abrir')}, 7000)
 }
+
+function abrirCurriculo(){
+    const janelacur = document.getElementById('janela-curriculo')
+    janelacur.classList.add('abrirc')
+
+    janelacur.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar-curriculo' || e.target.id == 'janela-curriculo'){
+            janelacur.classList.remove('abrirc')
+        }
+    })
+
+
+
+    // setTimeout(function () {modal.classList.remove('abrir')}, 7000)
+}
