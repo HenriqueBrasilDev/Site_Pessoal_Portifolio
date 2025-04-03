b6870f3b00c044190a797ebe468569e741eefceb
HenriqueBrasilDev <henrique_brasiloficial@outlook.com>
2025-04-03 01:23:46 -0300

    adicionando GSAP

---
+++ gsap.js
@@ -0,0 +1,34 @@
+gsap.from("h1",{
+    y:-100,
+    duration:1
+})
+
+gsap.from(".btt-port",{
+    y:100,
+    duration:1
+})
+
+gsap.from(".btt-cur",{
+    y:100,
+    duration:2
+})
+
+gsap.from(".btt-whats",{
+    y:100,
+    duration:3
+})
+
+gsap.from(".instagram",{
+    x:-100,
+    duration:2
+})
+
+gsap.from(".github",{
+    y:100,
+    duration:3
+})
+
+gsap.from(".linkedin",{
+    x:100,
+    duration:2
+})
