7452b0d03c51aefabfc26837e1c40715e8773c64
Henrique Brasil <henrique_brasiloficial@outlook.com>
2025-07-12 15:50:55 -0300

    Commit conserto das animações de entrada

--- gsap.js
+++ gsap.js
@@ -4,37 +4,37 @@ gsap.from("h1",{
 })
 
 gsap.from(".btt-port",{
-    y:200,
+    y:700,
     duration:2,
     delay: 1
 })
 
 gsap.from(".btt-cur",{
-    y:200,
+    y:700,
     duration:2,
     delay: 2
 })
 
 gsap.from(".btt-whats",{
-    y:200,
+    y:700,
     duration:2,
     delay: 3
 })
 
 gsap.from(".instagram",{
-    x:-200,
+    x:-300,
     duration:2,
     delay: 5
 })
 
 gsap.from(".github",{
-    y:100,
+    y:200,
     duration:3,
     delay: 3
 })
 
 gsap.from(".linkedin",{
-    x:200,
+    x:300,
     duration:2,
     delay: 5
 })
