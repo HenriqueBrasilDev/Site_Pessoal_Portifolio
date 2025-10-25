76c7fb6d530e106a7e61e3f97ce100d668438c79
Henrique Brasil <henrique_brasiloficial@outlook.com>
2025-10-24 22:26:22 -0300

    ...

--- main.js
+++ main.js
@@ -20,80 +20,77 @@ if (nextBtn) {
 
 
 
+    let runTimeOut
 
-
-let runTimeOut
-
-let runNextAuto = setTimeout(() => {
-    nextBtn.click()
-}, timeAutoNext)
-
-
-
-
-function resetTimeAnimation (){
-    runningTime.style.animation = 'nome'
-    runningTime.offsetHeight
-    runningTime.style.animation = null
-    runningTime.style.animation = 'runningTime 7s linear 1 forwards'
-}
-
-
-
-
-
-
-
-
-
-function showSlider(type) {
-    let sliderItemsDom = list.querySelectorAll('.projetos .carousel .carousel-item')
-    if(type ==='next'){
-        list.appendChild(sliderItemsDom[0])
-        carousel.classList.add('next')
-    } else{
-        list.prepend(sliderItemsDom[sliderItemsDom.length - 1])
-        carousel.classList.add('prev')
-    }
-
-    clearTimeout(runTimeOut)
-
-    runTimeOut = setTimeout( () => {
-        carousel.classList.remove('next')
-        carousel.classList.remove('prev')
-    }, timeRunning)
-
-    clearTimeout(runNextAuto)
-    runNextAuto = setTimeout(() => {
+    let runNextAuto = setTimeout(() => {
         nextBtn.click()
     }, timeAutoNext)
+
+
+
+    function resetTimeAnimation (){
+        runningTime.style.animation = 'nome'
+        runningTime.offsetHeight
+        runningTime.style.animation = null
+        runningTime.style.animation = 'runningTime 7s linear 1 forwards'
+    }
+
+
+
+    function showSlider(type) {
+        let sliderItemsDom = list.querySelectorAll('.projetos .carousel .carousel-item')
+        if(type ==='next'){
+            list.appendChild(sliderItemsDom[0])
+            carousel.classList.add('next')
+        } else{
+            list.prepend(sliderItemsDom[sliderItemsDom.length - 1])
+            carousel.classList.add('prev')
+        }
+
+        clearTimeout(runTimeOut)
+
+        runTimeOut = setTimeout( () => {
+            carousel.classList.remove('next')
+            carousel.classList.remove('prev')
+        }, timeRunning)
+
+        clearTimeout(runNextAuto)
+        runNextAuto = setTimeout(() => {
+            nextBtn.click()
+        }, timeAutoNext)
+
+        resetTimeAnimation()
+    }
 
     resetTimeAnimation()
 }
 
-resetTimeAnimation()
-}
+
 
 
 
 if (document.querySelector(".hard-skills")) {
     let indexx = 0;
     const carousel = document.querySelectorAll(".hard-skills li");
-
-    const destaque1 = document.querySelector(".hard-skills .li").length;
-    const destaque2 = document.querySelector(".hard-skills .li p.selected");
-    const destaque3 = document.querySelector(".hard-skills .li h3.selected");
-
+    
+    // const destaque1 = document.querySelector(".hard-skills .li").length;
+    // const destaque2 = document.querySelector(".hard-skills .li p");
+    // const destaque3 = document.querySelector(".hard-skills .li h3");
+    
     setInterval(() => {
         indexx = (indexx + 1) % carousel.length;
         
+        carousel.forEach(item => item.classList.remove('selected'));
+
+        carousel[indexx+3].classList.add('selected');
+
         carousel.forEach((item, i) => {
-            item.style.transform = `translateX(-${indexx * 100}%)`;
+            item.style.transform = `translateX(-${indexx * 100}px)`;
         });
 
-        destaque1.classList.add("selected");
-        destaque2.classList.add("selected");
-        destaque3.classList.add("selected");
+        
+        // destaque2.classList.add("selected");
+        // destaque3.classList.add("selected");
 
     }, 4000);
 }
